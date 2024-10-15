6d4d2b29be41ed0de629cd6f821c965e47997185
SJU <soulsdhed@gmail.com>
2024-10-15 10:32:44 +0900

    init

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 10,
+  waitForConnections: true,
+  queueLimit: 0,
+});
+
+db.getConnection((err, connection) => {
+  if (err) throw err;
+  console.log("MySQL Connected...");
+  connection.release();
+});
+
+module.exports = db;
