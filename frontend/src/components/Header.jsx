9a4d8c1c75dcbbaa23f47492e7567bf0ca899d12
Insa5_JSA_hacksim_3 <ko0413hee@gmail.com>
2024-10-23 01:07:48 +0900

    wonhee-mypage

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import '../assets/css/header.css'
+import React, { useState } from 'react';
+import '../assets/css/header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
+  const [menuDisplay, setMenuDisplay] = useState('none'); 
+
+  const toggleMenu = () => {
+    setMenuDisplay(prevDisplay => (prevDisplay === 'none' ? 'block' : 'none'));
+  };
+
   return (
     <header>
       <div className='site-header'>
         <div className='header-container'>
           <a href="/"><h1>reciFIT</h1></a>
-        
-          {/* Navication */}
+
+          {/* Navigation */}
           <nav id='site-navigation'>
             <div className='menu-container'>
               <ul className='menu-list'>
@@ -22,26 +30,38 @@ const Header = () => {
         </div>
 
         <div className='siteFamily'>
-          <a href="javascript:void(0)" class="SiteFamily-text">마이페이지<span class="SiteFamily-bar"></span></a>
-          <ul class="SiteFamilySelect">
-            <li class='SiteFamilySelect-item'><a href='http://blog.chungjungone.com/' class='SiteFamilySelect-link' target='_blank'>회원정보 수정</a></li>
-            <li class='SiteFamilySelect-item'><a href='http://www.jongga.co.kr' class='SiteFamilySelect-link' target='_blank'>추가 정보 입력/수정</a></li>
-            <li class='SiteFamilySelect-item'><a href='http://www.daesang.com/' class='SiteFamilySelect-link' target='_blank'>나의 레시피 조회</a></li>
-            <li class='SiteFamilySelect-item'><a href='http://www.jungoneshop.com/' class='SiteFamilySelect-link' target='_blank'>현재 재고 관리</a></li>
-            <li class='SiteFamilySelect-item'><a href='http://www.wellife.co.kr/' class='SiteFamilySelect-link' target='_blank'>영수증 조회 및 관리</a></li>				
+          <a href="#" className="SiteFamily-text" onClick={toggleMenu}>
+            <FontAwesomeIcon icon={faUser} id='userFont' /> 마이페이지
+            <span className="SiteFamily-bar"></span>
+          </a>
+          <ul className="SiteFamilySelect" style={{ display: menuDisplay }}>
+            <li className='SiteFamilySelect-item'>
+              <a href='http://blog.chungjungone.com/' className='SiteFamilySelect-link' target='_blank' rel="noopener noreferrer">회원정보 수정</a>
+            </li>
+            <li className='SiteFamilySelect-item'>
+              <a href='http://www.jongga.co.kr' className='SiteFamilySelect-link' target='_blank' rel="noopener noreferrer">추가 정보 입력/수정</a>
+            </li>
+            <li className='SiteFamilySelect-item'>
+              <a href='http://www.daesang.com/' className='SiteFamilySelect-link' target='_blank' rel="noopener noreferrer">나의 레시피 조회</a>
+            </li>
+            <li className='SiteFamilySelect-item'>
+              <a href='http://www.jungoneshop.com/' className='SiteFamilySelect-link' target='_blank' rel="noopener noreferrer">현재 재고 관리</a>
+            </li>
+            <li className='SiteFamilySelect-item'>
+              <a href='http://www.wellife.co.kr/' className='SiteFamilySelect-link' target='_blank' rel="noopener noreferrer">영수증 조회 및 관리</a>
+            </li>
           </ul>
-				</div>
+        </div>
 
-        {/* <!-- Mobile Btn -->
-        <button class="MobileMenuBtn">
-          <span class="MobileMenuBtn-line"></span>
-          <span class="MobileMenuBtn-line"></span>
-          <span class="MobileMenuBtn-line"></span>
-        </button>
-        <!-- .Mobile Btn --> */}
+        {/* Mobile Btn */}
+        {/* <button className="MobileMenuBtn">
+          <span className="MobileMenuBtn-line"></span>
+          <span className="MobileMenuBtn-line"></span>
+          <span className="MobileMenuBtn-line"></span>
+        </button> */}
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -13,18 +13,23 @@ const Main = () => {
             <div className="Main-container">
               <div className="MainSwipe">
                 <h2 className="MainSwipeTitle First">The Better</h2>
-                <div className="MainSwipeThumb">
-                  <div className="MainSwipeThumbSlide">
-                    <img className="MainSwipeThumbSlide-img" src="https://www.chungjungone.com/wp-content/uploads/2022/03/brand-main-img1.png" alt="" />
-                    {/* Add other images */}
+                <div class="MainSwipeThumb">
+                  <div class="MainSwipeThumbSlide">
+                    {/* <img class='MainSwipeThumbSlide-img' src='https://www.chungjungone.com/wp-content/uploads/2022/03/brand-main-img1.png' alt='' /> */}
+                    <img class='MainSwipeThumbSlide-img' src='/img/MainSwipeThumbSlide2.png' alt='' />
+                    <img class='MainSwipeThumbSlide-img' src='/img/2.png' alt='' />
+                    {/* <img class='MainSwipeThumbSlide-img' src='https://www.chungjungone.com/wp-content/uploads/2022/04/brand-main-img3.png' alt='' /> */}
+                    <img class='MainSwipeThumbSlide-img' src='/img/3.png' alt='' />
+                    <img class='MainSwipeThumbSlide-img' src='/img/01.png' alt='' />
+                    <img class='MainSwipeThumbSlide-img' src='/img/1.png' alt='' />					
                   </div>
                 </div>
                 <h2 className="MainSwipeTitle Last">For you Only</h2>
               </div>
             </div>
-            <a href="#;" className="MainTopBannerScroll">
+            <a href="#;" className="MainTopBannerScroll" rel="noopener noreferrer">
               <span className="MainTopBannerScroll-ico"><span className="MainTopBannerScroll-bg"></span></span>
-              <span className="MainTopBannerScroll-text">Go</span>
+              <span className="MainTopBannerScroll-text"></span>
             </a>
           </div>
           {/* Additional sections */}
