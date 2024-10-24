4df15d7f0d5fbb2c47440edb781d59e6ac553a2a
ash <ansh0827@naver.com>
2024-10-24 18:44:50 +0900

    안수현커밋

--- frontend/src/pages/Join.jsx
+++ frontend/src/pages/Join.jsx
@@ -3,14 +3,15 @@ import '../assets/css/join.css'
 
 
 
-const images = [
-    `${process.env.PUBLIC_URL}/img/login_pic/login_pic1.jpg`,
-    `${process.env.PUBLIC_URL}/img/login_pic/login_pic2.jpg`,
-    `${process.env.PUBLIC_URL}/img/login_pic/login_pic3.jpg`,
-    `${process.env.PUBLIC_URL}/img/login_pic/login_pic4.jpg`
-];
-
 const Join = () => {
+
+// 페이드인, 아웃 이미지---------------------------------------------------
+    const images = [
+        `${process.env.PUBLIC_URL}/img/login_pic/login_pic1.jpg`,
+        `${process.env.PUBLIC_URL}/img/login_pic/login_pic2.jpg`,
+        `${process.env.PUBLIC_URL}/img/login_pic/login_pic3.jpg`
+    ];
+
     const [currentIndex, setCurrentIndex] = useState(0);
     const [fade, setFade] = useState(true);
 
@@ -23,17 +24,28 @@ const Join = () => {
             setTimeout(() => {
                 setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
                 setFade(true); // Fade-in
-            }, 800); // fade-out 시간과 일치
+            }, 1000); // fade-out 시간과 일치
 
-        }, 4000); // 4초마다 이미지 전환
+        }, 8000); // 8초마다 이미지 전환
 
         return () => clearInterval(interval); // 컴포넌트 언마운트 시 정리
     }, []);
 
 
+// 카카오 로그인---------------------------------------------------
+    const REST_API_KEY = '백엔드한테 달라하자1';
+    const REDIRECT_URI = '백엔드한테 달라하자2';
+    const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+    const loginHandler = () => {
+        window.location.href = link;
+    };
+
+
+
 
     return (
-        <div className='container'> 
+        <div className='container'>
             <div className="slider-container">
                 <img
                     src={images[currentIndex]}
@@ -43,8 +55,26 @@ const Join = () => {
             </div>
 
             <div className='login'>
-                <img src={`${process.env.PUBLIC_URL}/img/login_pic/logo.png`} alt="로고" className='login-logo'/>
+                <img src={`${process.env.PUBLIC_URL}/img/login_pic/logo.png`}
+                    alt="로고" className='login-logo' />
+
+                <h3 className='login-small-text'> sns 로그인으로 간편하게 
+                    <br/>레시핏 서비스를 즐겨보세요</h3>
+
+                <div className='Query-container'>
+                <img
+                    src={images[currentIndex]}
+                    alt={`Slide ${currentIndex + 1}`}
+                    className={`query-slider-image ${fade ? 'fade-in' : 'fade-out'} mediaQuery-slider`}
+                />
+                </div>
+
                 <h1 className='login-text'>로그인</h1>
+
+                <img src={`${process.env.PUBLIC_URL}/img/login_pic/kakao_login_medium_wide.png`} alt="카카오로그인"
+                    onClick={loginHandler} className='kakao-button'/>
+
+
             </div>
         </div>
     );
