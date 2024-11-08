4149413e6ad8ff3426d7fd9fd2491d17c3b6c3d5
SJU <soulsdhed@gmail.com>
2024-11-08 09:47:06 +0900

    recipe router

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -49,19 +49,28 @@ const isStartDateBeforeEndDate = (start, end) => {
   return new Date(start) <= new Date(end);
 };
 
-const isValidURL = (url) => {
-  const urlPattern = new RegExp(
-    "^(https?:\\/\\/)" + // protocol
-      "((([a-zA-Z\\d]([a-zA-Z\\d-]*[a-zA-Z\\d])*)\\.?)+[a-zA-Z]{2,}|" + // domain name
-      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-      "(\\:\\d+)?" + // port
-      "(\\/[-a-zA-Z\\d%@_.~+&:]*)*" + // path
-      "(\\?[;&a-zA-Z\\d%@_.,~+&:=-]*)?" + // query string
-      "(\\#[-a-zA-Z\\d_]*)?$", // fragment locator
-    "i" // case insensitive
-  );
+// const isValidURL = (url) => {
+//   const urlPattern = new RegExp(
+//     "^(https?:\\/\\/)" + // protocol
+//       "((([a-zA-Z\\d]([a-zA-Z\\d-]*[a-zA-Z\\d])*)\\.?)+[a-zA-Z]{2,}|" + // domain name
+//       "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+//       "(\\:\\d+)?" + // port
+//       "(\\/[-a-zA-Z\\d%@_.~+&:]*)*" + // path
+//       "(\\?[;&a-zA-Z\\d%@_.,~+&:=-]*)?" + // query string
+//       "(\\#[-a-zA-Z\\d_]*)?$", // fragment locator
+//     "i" // case insensitive
+//   );
 
-  return !!urlPattern.test(url);
+//   return !!urlPattern.test(url);
+// };
+
+const isValidURL = (string) => {
+  try {
+    new URL(string);
+    return true;
+  } catch (_) {
+    return false;
+  }
 };
 
 const isValidEmail = (email) => {
@@ -152,10 +161,15 @@ const isValidNickname = (nickname) => {
 };
 
 // 재료 조건
-function isValidIngredientsFormat(ingredients) {
+const isValidIngredientsFormat = (ingredients) => {
   // 쉼표로 구분된 하나 이상의 항목이 있는지 확인
   const formatRegex = /^[^,]+(, [^,]+)*$/;
   return formatRegex.test(ingredients);
+};
+
+// Idx 확인
+function isValidIdx(idx) {
+  return Number.isInteger(Number(idx)) && !isNaN(Number(idx));
 }
 
 module.exports = {
@@ -170,4 +184,5 @@ module.exports = {
   isValidUserId,
   isValidNickname,
   isValidIngredientsFormat,
+  isValidIdx,
 };
