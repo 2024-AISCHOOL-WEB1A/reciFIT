75ce987ad09b556705a57cfe09b55d3fb63d2b96
wonnie <ko0413hee@gmail.com>
2024-11-12 21:03:08 +0900

    wonhee recipeDetail update

--- frontend/src/pages/RecipeDetail.jsx
+++ frontend/src/pages/RecipeDetail.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from 'react';
 import '../assets/css/recipe.css';
 
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleLeft, faUsers, faHourglassStart, faStar } from '@fortawesome/free-solid-svg-icons';
-// import { updateTitle } from './redux/reducers/createSlice';
-
+import { faAnglesLeft, faUsers, faHourglassStart, faStar } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 const RecipeDetail = () => {
+    const navigate = useNavigate();
 
-    const dispatch = useDispatch();
+    // 아이콘 클릭 시 이전 페이지로 되돌아가기
+    const goBack = () => {
+        navigate(-1); // -1은 이전 페이지로 돌아감
+    };
 
     const {
         photoSrc,
@@ -22,56 +25,115 @@ const RecipeDetail = () => {
         description
     } = useSelector((state) => state.recipe);
 
+    const sections = ingredients
+        .split("[")
+        .filter(Boolean)
+        .map((section) => section.trim());
+
     return (
         <div id="recipe">
             <div id="recipe-details-title">
-                <div id="recipe-details-goback">
-                    <FontAwesomeIcon icon={faAngleLeft} />
+                <div id="recipe-details-goback" onClick={goBack}>
+                    <FontAwesomeIcon icon={faAnglesLeft} />
                 </div>
             </div>
 
             <div className='detail-container'>
-                {/* Recipe Photo */}
                 <div className="photo">
                     <img id="photo" src={photoSrc} alt="Recipe Photo" />
                 </div>
 
-                {/* Recipe Name */}
-                <div id="recipe_name">{recipeName}</div>
+                <div className='instruct-container'>
+                    <div id="recipe_name">{recipeName}</div>
+                    <div id="instruction">{instructions}</div>
 
-                {/* Introduction */}
-                <div id="instruction">{instructions}</div>
-
-                {/* Additional Information */}
-                <div className="rowflex">
-                    <div className="recipe-detail-rowflex-div">
-                        <FontAwesomeIcon icon={faUsers} />
-                        <div>{servings} 인분</div>
-                    </div>
-                    <div className="recipe-detail-rowflex-div">
-                        <FontAwesomeIcon icon={faHourglassStart} />
-                        <div>{time} 소요시간</div>
-                    </div>
-                    <div className="recipe-detail-rowflex-div">
-                        <FontAwesomeIcon icon={faStar} />
-                        <div>{difficulty} 난이도</div>
+                    {/* Additional Information */}
+                    <div className="rowflex">
+                        <div className="recipe-detail-rowflex-div">
+                            <FontAwesomeIcon icon={faUsers} />
+                            <div>{servings} 인분</div>
+                        </div>
+                        <div className="recipe-detail-rowflex-div">
+                            <FontAwesomeIcon icon={faHourglassStart} />
+                            <div>{time} 소요시간</div>
+                        </div>
+                        <div className="recipe-detail-rowflex-div">
+                            <FontAwesomeIcon icon={faStar} />
+                            <div>{difficulty} 난이도</div>
+                        </div>
                     </div>
                 </div>
+            </div>
 
-                {/* Ingredients */}
-                <div id="ingredient-wrapper">
-                    {ingredients && ingredients.map((ingredient, index) => (
-                        <div key={index}>{ingredient}</div>
-                    ))}
+            {/* Ingredients */}
+            <div id="ingredient-wrapper">
+                {sections.map((section, index) => {
+                    const [title, ...items] = section.split("]");
+                    return (
+                        <div key={index} id='ingredient-List'>
+                            {/* 재료 제목 */}
+                            <div className='ingredientList-Title'>
+                                <h3>{title}</h3>
+                                <span>Ingredient</span>
+                            </div>
+
+                            {/* 재료 항목 테이블 */}
+                            <table>
+                                {items
+                                    .join(" ")
+                                    .split("|")
+                                    .map((item, idx) => {
+                                        const trimmedItem = item.trim();
+                                        const row = [];
+
+                                        if (trimmedItem.indexOf(" ") === -1) {
+                                            // 항목이 공백을 포함하지 않으면 한 열만 생성
+                                            row.push(
+                                                <th key={idx} style={{ textAlign: 'left' }}>
+                                                    {trimmedItem}
+                                                </th>
+                                            );
+                                            row.push(
+                                                <td key={idx + "second"} style={{ textAlign: 'center' }}>
+                                                    {}
+                                                </td>
+                                            );
+                                        } else {
+                                            // 항목이 공백을 포함하면 두 열로 나누기
+                                            const lastSpaceIndex = trimmedItem.lastIndexOf(" ");
+                                            const firstPart = trimmedItem.substring(0, lastSpaceIndex).trim();
+                                            const secondPart = trimmedItem.substring(lastSpaceIndex + 1).trim();
+
+
+                                            row.push(
+                                                <th key={idx + "first"} style={{ textAlign: 'left' }}>
+                                                    {firstPart}
+                                                </th>
+                                            );
+                                            row.push(
+                                                <td key={idx + "second"} style={{ textAlign: 'center' }}>
+                                                    {secondPart}
+                                                </td>
+                                            );
+                                        }
+                                        return <tr key={idx}>{row}</tr>;
+                                    })}
+                            </table>
+                        </div>
+                    );
+                })}
+            </div>
+
+            {/* Description */}
+            <div id='description-wrapper'>
+                <div id="description-title">
+                    <h3>요리 방법</h3>
+                    <span>Cooking</span>
                 </div>
-
-                {/* Description */}
-                <div id="description-title">요리 방법</div>
-                <div id="description">{description}</div>
-
+                <div className="description">{description}</div>
             </div>
         </div>
     );
 }
 
-export default RecipeDetail
+export default RecipeDetail;
