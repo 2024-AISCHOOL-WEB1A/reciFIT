5ec8ee1e79bc194dab895b2db11f33661c261179
ash <ansh0827@naver.com>
2024-11-12 10:31:31 +0900

    ash

--- frontend/src/pages/Receipt.jsx
+++ frontend/src/pages/Receipt.jsx
@@ -1,35 +1,38 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../assets/css/receipt.css'
 import receiptData from '../json/receiptData.json'
 
 const Receipt = () => {
-    const [selectedItems, setSelectedItems] = useState(new Array(receiptData.length).fill(false));
+    // const [selectedItems, setSelectedItems] = useState(new Array(receiptData.length).fill(false));
 
-    const handleCheckbox = (index) => {
-        const updatedSelectedItems = [...selectedItems];
-        updatedSelectedItems[index] = !updatedSelectedItems[index];
-        setSelectedItems(updatedSelectedItems);
-    };
+    // const handleCheckbox = (index) => {
+    //     const updatedSelectedItems = [...selectedItems];
+    //     updatedSelectedItems[index] = !updatedSelectedItems[index];
+    //     setSelectedItems(updatedSelectedItems);
+    // };
 
 
     return (
         <form className='receipt-container'>
-            {/* <div className='receipt-bannercon'><img src={`${process.env.PUBLIC_URL}/img/receipt_bg.jpg`}  className='receipt-banner'alt="" /></div> */}
-
-            {/* 왼쪽 영수증 사진 */}
             <div className='receipt'>
                 <div className='receipt-pic'>
-                    <img src={`${process.env.PUBLIC_URL}/img/receipt_img/receipt2.png`} alt=""
-                        className='receipt-img' />
+                    <h2 style={{ color: 'white', paddingBottom: '10%' }}>picture</h2>
+                    <img src={`${process.env.PUBLIC_URL}/img/receipt_img/receipt2.png`} alt="" className='receipt-img' />
                 </div>
 
                 {/* 오른쪽 인식 텍스트 */}
-                <div className='receipt-textContainer'>
-                    <img src={`${process.env.PUBLIC_URL}/img/receipt_img/Subtract.png`} alt=""
-                        className='receipt-top' />
+                <div className='receipt-right'>
+                    {/* 영수증 출입구 일러 */}
+                    <div className='receipt-door'></div>
+                    {/* 영수증 일러 */}
+                    <div className='receipt-illustration'>
+                        <div className='receipt-title'> <h2>Receipt</h2> </div>
+                        <div style={{ width: '100%', margin: '3% 0' }}> ******************************** </div>
+                        <div style={{ display: 'flex' }}>
+                            <span className='receipt-nick'> 상냥한 고비 님 </span>
+                            <span className='receipt-date'>2024 / 11 / 11</span>
+                        </div>
 
-                    <div className='receipt-box'>
-                        <div className='receipt-boxTitle'> <h2>인식결과</h2> </div>
                         <div className='receipt-subTitle'>
                             <div className='receipt-subname'>상품명</div>
                             <div className='receipt-subquantity'>수량</div>
@@ -37,32 +40,23 @@ const Receipt = () => {
 
                         {receiptData.map((item, index) => (
                             <div key={index} className='receipt-boxMiddle'>
-                                <input type="checkbox" className="receipt-checkbox"
-                                    checked={selectedItems[index]}
-                                    onChange={() => handleCheckbox(index)} />
                                 <div className='receipt-name'>{item.name}</div>
                                 <div className='receipt-quantity'>{item.quantity}</div>
                             </div>
-                        ))}
+                        ),
 
-                        <div className='receipt-info'>
-                            영양분 정보
-                        </div>
-                        <div>
-
-                        </div>
+                        )}
                     </div>
+                    <div className='receipt-info'>영양분 정보</div>
                 </div>
 
                 <div className='receipt-CRUD'>
                     <button className='receipt-R'> 확인 </button>
                     <button className='receipt-U'> 수정 </button>
-                    <button className='receipt-C'> 추가 </button>
-                    <button className='receipt-D'> 삭제 </button>
                 </div>
             </div>
 
-        </form>
+        </form >
     )
 }
 
