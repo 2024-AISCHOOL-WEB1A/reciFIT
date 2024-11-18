1fc24924dacabfeb5353db0f6728c6cb0a5d953f
SJU <soulsdhed@gmail.com>
2024-11-18 16:32:19 +0900

    mongodb

--- ocr-server/app.py
+++ ocr-server/app.py
@@ -20,16 +20,19 @@ SECRET_KEY = os.getenv("CLOVA_SECRET_KEY")
 API_URL = os.getenv("CLOVA_ENDPOINT")
 PORT = int(os.getenv("PORT", 8000))
 
-MONGODB_URI = os.getenv("MONGODB_URI")
-MONGODB_DB = os.getenv("MONGODB_DB")
+MONGODB_URL = os.getenv('MONGODB_URL')
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
+MONGODB_PORT = os.getenv('MONGODB_PORT')
+MONGODB_USER = os.getenv('MONGODB_USER')
+MONGODB_DATABASENAME = os.getenv('MONGODB_DATABASENAME')
+MONGODB_URI = f"mongodb://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_URL}:{MONGODB_PORT}/{MONGODB_DATABASENAME}"
 
 app = FastAPI()
 
-# TODO : 몽고 DB 설정 다시 확인
-# MongoDB 클라이언트 설정
-# client = MongoClient(MONGODB_URI)
-# db = client[MONGODB_DB]
-# ocr_collection = db["ocr_results"]
+# 몽고 DB 연결
+mongodb_client = MongoClient(MONGODB_URI)
+db = mongodb_client[MONGODB_DATABASENAME]
+collection = db['ocr_results']
 
 # 요청 데이터 모델 정의
 class OCRRequest(BaseModel):
@@ -62,19 +65,19 @@ async def perform_ocr(request: OCRRequest):
         headers = {'X-OCR-SECRET': SECRET_KEY}
 
         # OCR 요청 전송
-        response = requests.post(API_URL, headers=headers, data=payload, files=files)
+        response = requests.post(API_URL, headers=headers, data=payload, files=files, timeout=30)
 
         # 응답 처리
         if response.status_code == 200:
             ocr_results = response.json()
             
-            #  # MongoDB에 결과 저장
-            # ocr_record = {
-            #     "photoUrl": request.photoUrl,
-            #     "ocrResults": ocr_results,
-            #     "timestamp": datetime.now(timezone.utc)
-            # }
-            # ocr_collection.insert_one(ocr_record)
+             # MongoDB에 결과 저장
+            ocr_record = {
+                "photoUrl": request.photoUrl,
+                "ocrResults": ocr_results,
+                "timestamp": datetime.now(timezone.utc)
+            }
+            collection.insert_one(ocr_record)
             
             return ocr_results
         else:
